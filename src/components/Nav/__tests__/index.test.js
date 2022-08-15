eca328bea5d0cd15c6f62fdb376a084eea614a69
Jennifer <goodwije@gmail.com>
2022-08-14 18:33:20 -0600

    feature/gallery

--- src/components/Nav/__tests__/index.test.js
+++ src/components/Nav/__tests__/index.test.js
@@ -2,20 +2,17 @@ import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Nav from '..';
-import { toHaveTextContent } from '@testing-library/jest-dom/dist/matchers';
 
 afterEach(cleanup);
 
 describe('Nav component', () => {
-  // baseline test
   it('renders', () => {
     render(<Nav />);
   });
 
-  // snapshot test
   it('matches snapshot', () => {
     const { asFragment } = render(<Nav />);
-    // assert value comparison
+
     expect(asFragment()).toMatchSnapshot();
   });
 });
@@ -31,6 +28,7 @@ describe('emoji is visible', () => {
 describe('links are visible', () => {
   it('inserts text into the links', () => {
     const { getByTestId } = render(<Nav />);
+
     expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
     expect(getByTestId('about')).toHaveTextContent('About me');
   });
